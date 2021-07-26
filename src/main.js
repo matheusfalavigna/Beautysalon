05d98c1bc3d4bff9694cbae4a8ceb32fcaaae909
Matheus Felipe Buffon Falavigna <matheusfalavigna@gmail.com>
2021-07-26 14:49:12 -0300

    Surface Exploration

--- src/main.js
+++ src/main.js
@@ -19,9 +19,10 @@ for (const link of links) {
 };
 
 // mudar o header da página quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header');
-    const navHeight = header.offsetHeight;
 
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll');
@@ -38,6 +39,12 @@ const swiper = new Swiper('.swiper-container', {
     },
     mousewheel: true,
     keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 // SCROLLREVEAL: mostrar elementos quando der scroll na página
@@ -58,8 +65,9 @@ scrollReveal.reveal(`
 `, { interval: 100 });
 
 // Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top');
+
 function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
 
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show');
@@ -68,8 +76,35 @@ function backToTop() {
     }
 }
 
+// Menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]');
+
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const sectionId = section.getAttribute('id');
+
+        const checkpointStart = checkpoint >= sectionTop;
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active');
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active');
+        }
+    };
+};
+
 // Quando se tem o rolagem(scrolling) pela página
 window.addEventListener('scroll', () => {
-    changeHeaderWhenScroll();
-    backToTop();
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
 });
